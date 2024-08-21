423b62cbb136ed355c9807af2e87605bb02f4f57
jafar-sadigzade <cefersadiq@gmail.com>
2024-08-21 10:31:18 +0400

    make some fixes

--- exam/urls.py
+++ exam/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.exam_request),
-    path('exam/<int:exam_id>/results/table', views.exam_results_table, name='exam_results_table'),
-    path('exam/<int:exam_id>/results/report', views.exam_results_report, name='exam_results_report'),
+    path('', views.index, name='index'),
+    path('exam/boy/report', views.boy_report, name='boy_report'),
+    path('exam/girl/report', views.girl_report, name='girl_report'),
+    path('exam/<int:exam_id>/results/table/girl', views.exam_results_table_girl, name='exam_results_table_girl'),
+    path('exam/<int:exam_id>/results/table/boy', views.exam_results_table_boy, name='exam_results_table_boy'),
+    path('exam/<int:exam_id>/results/report/girl', views.exam_results_report_girl, name='exam_results_report_girl'),
+    path('exam/<int:exam_id>/results/report/boy', views.exam_results_report_boy, name='exam_results_report_boy'),
 ]
