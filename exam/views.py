9b92d23714c19b7121154781d1c12ad55bad3904
jafar-sadigzade <cefersadiq@gmail.com>
2024-08-23 02:34:58 +0400

    add pdf

--- exam/views.py
+++ exam/views.py
@@ -9,27 +9,44 @@ def index(request):
 
 
 def get_student_report(exam, student_id, gender, template_name):
-    img = f'/static/pdf/{student_id}.pdf'
+    pdf = f'/static/pdf/{student_id}.pdf'
 
     try:
+        # Get the list of students ordered by total_score
         students_ordered = Student.objects.filter(exam=exam, gender=gender).order_by('-total_score')
         student = students_ordered.get(student_id=student_id)
         student_index = list(students_ordered).index(student) + 1
+
+        # Check the score of the 30th student
+        if student_index <= 30:
+            message = "Qəbul oldunuz!"
+            tags = "alert-success"
+        else:
+            thirtieth_student = students_ordered[29]  # 30th student (index 29 because it's 0-based index)
+            if student.total_score == thirtieth_student.total_score:
+                message = "Qəbul oldunuz!"
+                tags = "alert-success"
+            else:
+                message = "Qəbul olmadınız!"
+                tags = "alert-danger"
+
         return {
             'student': student,
             'rank': student_index,
-            'img': img
+            'pdf': pdf,
+            'message': message,
+            "tags": tags
         }
     except Student.DoesNotExist:
-        return {'error': 'İş nömrəsi tapılmadı! '}
+        return {'error': 'İş nömrəsi tapılmadı!'}
     except Exam.DoesNotExist:
-        return {'error': 'İmtahan tapılmadı! '}
+        return {'error': 'İmtahan tapılmadı!'}
 
 
 def report_view(request, gender, template_name):
     if request.method == 'POST':
         student_id = request.POST.get('student_id')
-        exam = get_object_or_404(Exam, id=1)
+        exam = get_object_or_404(Exam, id=2)
         context = get_student_report(exam, student_id, gender, template_name)
         return render(request, template_name, context)
     return render(request, 'exam/input_id.html')
